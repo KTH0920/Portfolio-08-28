bd36b082d2bb9d49898f055eff1ae120e83f7a23
KTH0920 <taehyun9482@gmail.com>
2025-09-01 21:37:40 +0900

    commit

--- frontend/src/components/home/Work.jsx
+++ frontend/src/components/home/Work.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import works from "../../utils/work";
+import "./styles/Work.scss";
 
 const Work = () => {
   return (
-    <div>Work</div>
-  )
-}
+    <div className="inner work-inner">
+      <h3 className="sub-tit-2">works</h3>
+      <ul className="work-list">
+        {works.map((work) => (
+          <li key={work.id}>
+            <a href={work.links.notion} className="lst-t-wrap">
+              <h4 className="lst-tit">
+                {work.title}
+              </h4>
+              <div className="tags">
+                {work.tags.map((t, i) => (
+                  <span key={i} > {t}</span>
+                ))}
+              </div>
+            </a>
+            <a href={work.links.demo} className="lst-img-wrap">
+              <div className="bg" style={{backgroundImage:`url(${work.thumbnail})`}}>
 
-export default Work
+              </div>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Work;
