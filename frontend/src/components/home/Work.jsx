e6f856b3401b0b1bb44eed791808a03381595ee5
KTH0920 <taehyun9482@gmail.com>
2025-09-02 21:14:05 +0900

    commit

--- frontend/src/components/home/Work.jsx
+++ frontend/src/components/home/Work.jsx
@@ -5,24 +5,24 @@ import "./styles/Work.scss";
 const Work = () => {
   return (
     <div className="inner work-inner">
-      <h3 className="sub-tit-2">works</h3>
-      <ul className="work-list">
+      <h3 className="sub-tit-2">works.</h3>
+      <ul className="work-lst">
         {works.map((work) => (
           <li key={work.id}>
             <a href={work.links.notion} className="lst-t-wrap">
-              <h4 className="lst-tit">
-                {work.title}
-              </h4>
+              <h4 className="lst-tit">{work.title}</h4>
+              <p className="lst-dsc">{work.description}</p>
               <div className="tags">
                 {work.tags.map((t, i) => (
-                  <span key={i} > {t}</span>
+                  <span key={i}>{t}</span>
                 ))}
               </div>
             </a>
             <a href={work.links.demo} className="lst-img-wrap">
-              <div className="bg" style={{backgroundImage:`url(${work.thumbnail})`}}>
-
-              </div>
+              <div
+                className="bg"
+                style={{ backgroundImage: `url(${work.thumbnail})` }}
+              ></div>
             </a>
           </li>
         ))}
